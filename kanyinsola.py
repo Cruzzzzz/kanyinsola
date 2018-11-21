5ececd2e3d921b6d8e449fe4ffde1e5d92508aad
Cruzzzzz <45172092+Cruzzzzz@users.noreply.github.com>
2018-11-21 14:14:34 +0100

    Add files via upload

---
+++ kanyinsola.py
@@ -0,0 +1,15 @@
+'''
+
+control and  conditional statment
+if condition
+'''
+a =('abdul') 
+b =('aye')
+if(a==b):
+    print('yes')
+elif(b=='aye'):
+    print('aye , abdul printed')
+elif(a=='abdul')
+    print('abdul printed')
+else:
+    print('no')
